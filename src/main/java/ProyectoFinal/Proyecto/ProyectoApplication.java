62a3a7f7d85f7a3efc1ddeaa38b3ca276885506d
HildaPech <72896837+HildaPech@users.noreply.github.com>
2021-11-25 15:30:57 -0500

    Nombre cambio

--- src/main/java/ProyectoFinal/Proyecto/ProyectoApplication.java
+++ src/main/java/ProyectoFinal/Proyecto/ProyectoApplication.java
@@ -1,10 +1,12 @@
 package ProyectoFinal.Proyecto;
+//FHEJYM
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoApplication {
+	//hola
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);

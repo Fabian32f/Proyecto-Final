7f79720ead0ff8971389c8af3384b92c5e9de857
EmilyBalam <72897197+EmilyBalam@users.noreply.github.com>
2021-12-04 10:17:28 -0600

    Seccion 5 Terminada
    TAREAS PRINCIPALES:
    -Implementar las tablas usuarios y authorities en la base de datos.
     -Implementar la clase Spring Security Config
    -Añadir configuración para el formLogin y logout
    -Añadir la vista personalizada login.html
    -Añadir los links del login y logout en el layout para iniciar y cerrar sesión

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,12 +1,16 @@
 package com.bolsadeideas.springboot.app;
 
+import org.springframework.context.annotation.Bean;
+
 // import java.nio.file.Paths;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,18 +18,27 @@ public class MvcConfig implements WebMvcConfigurer {
 	
 	//private final Logger log = LoggerFactory.getLogger(getClass());
 
-	@Override
+	/*@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 
-		/*String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
 		log.info(resourcePath);
 		
 		registry.addResourceHandler("/uploads/**")
 		.addResourceLocations(resourcePath);
-		*/
+		
+	}*/
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error_403").setViewName("error_403");
 	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	
 	
 
--- src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.bolsadeideas.springboot.app.models.service.IUploadFileService;
-
 @SpringBootApplication
 public class SpringBootDataJpaApplication  implements CommandLineRunner {
 
@@ -22,6 +22,13 @@ public class SpringBootDataJpaApplication  implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		uploadFileService.deleteAll();
 		uploadFileService.init();
+		
+        /* String password = "123";
+		
+		for(int i=0; i<2; i++) {
+			String bcryptPassword = passwordEncoder.encode(password);
+			System.out.println(bcryptPassword);
+		}*/
 	}
 
 }
